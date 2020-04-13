1abca5138def2cf48dd2e9fba9b1294baf4cdfdf
akrukovich <anatoliinakrukovich@gmail.com>
2020-04-13 17:37:40 +0300

    initial commit4

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config["SECRET_KEY"]='asdasdasdasd'
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+app.run()
